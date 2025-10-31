a48c047474576f8a22bdd805096ce3b2cb894532
ivanglydov <ivanglydov@yandex.ru>
2025-10-31 17:19:48 +0500

    SeparatedVisualInstruments

--- Assets/GearMind/Scripts/Instruments/Laser/Laser.cs
+++ Assets/GearMind/Scripts/Instruments/Laser/Laser.cs
@@ -5,8 +5,10 @@ public class Laser : MonoBehaviour
     [SerializeField] private LineRenderer LineRenderer;
     [SerializeField] private int MaxDistance;
     [SerializeField] private Transform StartLaserPoint;
+    [SerializeField] private float OffsetDrawLaser;
     [SerializeField] private float LaserWidth = 0.1f;
     public int LayerMask = 3;
+    private Vector3 OffsetDrawLaserVector;
 
 
 
@@ -17,13 +19,13 @@ public class Laser : MonoBehaviour
         var hit = Physics2D.Raycast(StartLaserPoint.position, transform.right, MaxDistance, 1 << LayerMask);
         if(hit.collider != null)
         {
-            LineRenderer.SetPosition(0, StartLaserPoint.position);
-            LineRenderer.SetPosition(1, hit.point);
+            LineRenderer.SetPosition(0, StartLaserPoint.position + OffsetDrawLaserVector);
+            LineRenderer.SetPosition(1, new Vector3(hit.point.x, hit.point.y, OffsetDrawLaser));
         }
         else
         {
-            LineRenderer.SetPosition(1, StartLaserPoint.position + transform.right * MaxDistance);
-            LineRenderer.SetPosition(0, StartLaserPoint.position);
+            LineRenderer.SetPosition(1, StartLaserPoint.position + transform.right * MaxDistance + OffsetDrawLaserVector);
+            LineRenderer.SetPosition(0, StartLaserPoint.position + OffsetDrawLaserVector);
 
         }
     }
@@ -35,6 +37,11 @@ public class Laser : MonoBehaviour
     {
         LineRenderer.startWidth = LaserWidth;
         LineRenderer.endWidth = LaserWidth;
+    }
+
+    public void Awake()
+    {
+        OffsetDrawLaserVector = new Vector3(0,0,OffsetDrawLaser);
     }
 
     public void Update()
