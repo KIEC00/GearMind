f9befd2bae21b256ef1b6d94ba430ecef39c554c
q4w3e2r1 <artem.zarafutdinov@urfu.me>
2025-11-08 22:30:00 +0500

    add new 5 levels

--- Assets/GearMind/Scripts/Instruments/Spring/Spring.cs
+++ Assets/GearMind/Scripts/Instruments/Spring/Spring.cs
@@ -11,13 +11,20 @@ public class Spring : MonoBehaviour
 
     [SerializeField]
     private float CaldownAfter = 0;
+    
     public bool IsCanPush { get; private set; } = true;
 
     private IEnumerator PushObject(GameObject gameObject)
     {
         IsCanPush = false;
         yield return new WaitForSeconds(CaldownBefore);
-        gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, Force), ForceMode2D.Impulse);
+        
+        // Получаем направление пружины (вверх по умолчанию)
+        Vector2 pushDirection = transform.up;
+        
+        // Применяем силу в направлении пружины
+        gameObject.GetComponent<Rigidbody2D>().AddForce(pushDirection * Force, ForceMode2D.Impulse);
+        
         yield return new WaitForSeconds(CaldownAfter);
         IsCanPush = true;
     }
