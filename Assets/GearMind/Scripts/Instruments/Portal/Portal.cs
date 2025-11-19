b3304a1ea4cb70e29413d980e9e1e5d8eff51002
Kiec00 <user.nullptr@gmail.com>
2025-11-19 18:30:57 +0500

    Add portal gizmos

--- Assets/GearMind/Scripts/Instruments/Portal/Portal.cs
+++ Assets/GearMind/Scripts/Instruments/Portal/Portal.cs
@@ -1,12 +1,20 @@
 using System.Collections;
+using System.Numerics;
 using Assets.GearMind.Instruments;
+using EditorAttributes;
 using UnityEngine;
 
 [SelectionBase]
 public class Portal : MonoBehaviour, IGameplayObject
 {
-    [SerializeField] private Portal _toPortal;
-    [SerializeField] private Collider2D _collider;
+    private static readonly WaitForSeconds _waitForSeconds0_1 = new(0.1f);
+
+    [SerializeField, Required]
+    private Portal _toPortal;
+
+    [Space]
+    [SerializeField]
+    private Collider2D _collider;
 
     private bool _isTeleporting;
 
@@ -27,8 +35,8 @@ public class Portal : MonoBehaviour, IGameplayObject
         if (_isTeleporting || _toPortal == null)
             return;
 
-        Rigidbody2D rb = other.GetComponent<Rigidbody2D>();
-        if (rb == null) return;
+        if (!other.TryGetComponent<Rigidbody2D>(out var rb))
+            return;
 
         _isTeleporting = true;
         _toPortal._isTeleporting = true;
@@ -45,12 +53,18 @@ public class Portal : MonoBehaviour, IGameplayObject
 
     private IEnumerator ResetTeleportFlagCoroutine()
     {
-        yield return new WaitForSeconds(0.1f);
+        yield return _waitForSeconds0_1;
 
         _isTeleporting = false;
         if (_toPortal != null)
             _toPortal._isTeleporting = false;
     }
+
+    private void OnDrawGizmosSelected()
+    {
+        if (!_toPortal)
+            return;
+        Gizmos.color = Color.aliceBlue;
+        Gizmos.DrawLine(transform.position, _toPortal.transform.position);
+    }
 }
-
-
