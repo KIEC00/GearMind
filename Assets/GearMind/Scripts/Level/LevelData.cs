d945e0ec311b759840ab7a1d090081bbb70fa28e
Kiec00 <user.nullptr@gmail.com>
2026-01-08 15:19:08 +0500

    remove level data has learning flag

--- Assets/GearMind/Scripts/Level/LevelData.cs
+++ Assets/GearMind/Scripts/Level/LevelData.cs
@@ -7,23 +7,20 @@ namespace Assets.GearMind.Level
     [Serializable]
     public struct LevelData
     {
+        public readonly int SceneID => _scene;
+        public readonly TutorialDataSO TutorialData => _tutorialData;
+        public readonly bool HasTutorial => _tutorialData != null;
+
         [SerializeField, SceneDropdown]
         private int _scene;
 
         [SerializeField]
-        private bool _hasTutorial;
-
-        [SerializeField]
         private TutorialDataSO _tutorialData;
+
         public LevelData(int sceneID)
         {
             _scene = sceneID;
-            _hasTutorial = false;
             _tutorialData = null;
         }
-
-        public int SceneID => _scene;
-        public bool HasTutorial => _hasTutorial;
-        public TutorialDataSO TutorialData => _tutorialData;
     }
 }
